13d8e140c6a3a1d9ee23b5b80c54bd58e58b1192
Jacob Atzen <jatzen@gmail.com>
2018-04-17 11:22:16 +0200

    Fix host

--- config/prod.exs
+++ config/prod.exs
@@ -63,7 +63,7 @@ config :chats, ChatsWeb.Endpoint,
   # http: [port: {:system, "PORT"}], # Uncomment this line if you are running Phoenix 1.2
   server: true, # Without this line, your app will not start the web server!
   secret_key_base: "${SECRET_KEY_BASE}",
-  url: [host: "example.com", port: 80],
+  url: [host: "impressive-fluid-seagull.gigalixirapp.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 config :chats, Chats.Repo,
